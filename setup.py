6ab7c8ff56179dcf8d630a26fc7b38a56b93547c
jegar-p <jegar@mirroranalytics.io>
2024-11-25 11:03:30 +1000

    Uploading public libraries for R and Python

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+name = 'ABSynthesis',
+version = '1.0',
+description = "Python wrapper for the ABSynthesis API",
+author = "Jac Davis and Jegar Pitchforth",
+author_email = "info@mirroranalytics.io",
+url = "mirroranalytics.io",
+py_modules=['ABSynthesis'])
